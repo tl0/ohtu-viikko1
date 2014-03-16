89121a34e39853450512d2f2a1dbe6033ba448df
tl0 <tl0@users.noreply.github.com>
2014-03-16 21:23:09 +0200

    rikki

--- OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ OhtuVarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -79,7 +79,7 @@ public class VarastoTest {
 
     @Test
     public void eiNegatiivistaOttoa() {
-        assertEquals(0, varasto.otaVarastosta(-2), vertailuTarkkuus);
+        assertEquals(1000, varasto.otaVarastosta(-2), vertailuTarkkuus);
     }
 
     @Test
